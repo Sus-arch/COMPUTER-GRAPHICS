8bd803d7df5999b813767074b621f641b3b77c30
Андрей <andrej5122785@yandex.ru>
2025-09-17 01:11:19 +0300

    Создание абстрактного класс для 3D фигур

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,8 +1,21 @@
+import numpy as np
 import tkinter as tk
+from abc import ABC, abstractmethod
+
+
+class Shape3D(ABC):
+    """Абстрактный базовый класс для 3D фигур"""
+
+    def __init__(self, name):
+        self.name = name
+        self.vertices = np.array([])  # вершины
+        self.edges = []  # ребра
+        self.faces = []  # грани
 
 
 class Rotation3DApp:
     def __init__(self):
+        # Создаем основное окно
         self.root = tk.Tk()
         self.root.title("Поворот объемного тела относительно осей координат на заданный угол.")
         self.root.geometry("1000x800")
@@ -10,13 +23,16 @@ class Rotation3DApp:
         self.setup_gui()
 
     def setup_gui(self):
+        # Создаем фрейм для графика
         frame_plot = tk.Frame(self.root)
         frame_plot.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=10, pady=10)
 
     def run(self):
+        """Запускает приложение"""
         self.root.mainloop()
 
 
 if __name__ == '__main__':
+    # Запуск графического приложения
     app = Rotation3DApp()
     app.run()
