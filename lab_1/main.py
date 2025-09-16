cc39103fa9bf23fa3844f8cf4b02318b5b773fad
Андрей <andrej5122785@yandex.ru>
2025-09-17 00:52:29 +0300

    Инициализация графического интерфейса

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,2 +1,22 @@
+import tkinter as tk
+
+
+class Rotation3DApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Поворот объемного тела относительно осей координат на заданный угол.")
+        self.root.geometry("1000x800")
+
+        self.setup_gui()
+
+    def setup_gui(self):
+        frame_plot = tk.Frame(self.root)
+        frame_plot.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+    def run(self):
+        self.root.mainloop()
+
+
 if __name__ == '__main__':
-    pass
+    app = Rotation3DApp()
+    app.run()
